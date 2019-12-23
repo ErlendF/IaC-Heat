48e9bfcd410187b4648fb0ec4c36d5cea7144bac
Erlend Fonnes <erlend.fonnes@gmail.com>
2019-12-23 13:00:09 +0100

    Updated link to github repo

--- lib/manager_boot.bash
+++ lib/manager_boot.bash
@@ -26,7 +26,7 @@ cat <<EOF > /var/tmp/r10k.pp
 class { 'r10k':
   sources => {
     'puppet' => {
-      'remote'  => 'https://bitbucket.org/ANTIOFF/control-repo-a-gruppe-7.git',
+      'remote'  => 'https://github.com/ErlendF/puppet-control-repo',
       'basedir' => '/etc/puppetlabs/code/environments',
       'prefix'  => false,
     },
